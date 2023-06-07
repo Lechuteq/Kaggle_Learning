48ffae57580b324b2e20ce07eecf2472999d0414
Lechuteq <112075793+Lechuteq@users.noreply.github.com>
2023-06-07 22:47:47 +0200

    Update test_bin.py

--- test_bin.py
+++ test_bin.py
@@ -8,14 +8,22 @@ def interlockable(a, b):
     #  <----  hajime!
     a=bin(int(a))
     b=bin(int(b))
+    out = True
 
     a_len=len(a)
     b_len=len(b)
 
-    print(f"a: {a}")
-    print(f"b: {b}")
-    i=max(a_len,b_len)
-    print(f"i: {i}")
+    #print(f"a: {a}")
+    #print(f"b: {b}")
+    k=max(a_len,b_len)-2
+    #print(f"i: {i}")
+
+    for i in range(k):
+        if a(i) is b(i):
+            out = True
+        else:
+            out = False
+    return out
 
 if __name__ == "__main__":
     main()
